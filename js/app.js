6d012a3bc004b8b5ad740e9ba33594d792294f05
xavier-joaobx <joao.batista91x@gmail.com>
2025-09-18 16:11:50 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,7 +10,11 @@ let financeChart = null;
 async function signUp() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-  const { error } = await supabase.auth.signUp({ email, password });
+  const { error } = await supabase.auth.signUp({ email, password ,
+                                               options: {
+      // garante que o link de confirmação redirecione para a pasta do projeto
+      emailRedirectTo: window.location.origin + "/Projeto-app-financas/"
+    }});
   if (error) alert("Erro no cadastro: " + error.message);
   else alert("Conta criada! Verifique seu email.");
 }
